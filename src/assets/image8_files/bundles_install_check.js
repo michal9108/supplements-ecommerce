d585a3c0561f06caab1d316a5be1624934bf2784
Michal Sedivka <sedivka9108@gmail.com>
2023-12-28 09:55:43 +0100

    deleting unnecessary files

--- src/assets/image8_files/bundles_install_check.js
+++
@@ -1,3 +0,0 @@
-window.BOLD = window.BOLD || {};
-window.BOLD.bundles = window.BOLD.bundles || {};
-window.BOLD.bundles.is_installed = true;
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,13 +30,9 @@ module.exports = {
       // TO DO 
       fontFamily: {
         dmsans: [ "sans-serif"],
-        montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
-        evolvetext: "url('./assets/EvolveText.png')",
-        abstractwaves: "url('./assets/AbstractWaves.png')",
-        sparkles: "url('./assets/Sparkles.png')",
-        circles: "url('./assets/Circles.png')",
+       
       },
     },
     screens: {
