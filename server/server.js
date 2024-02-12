db26a9a4314ad7793896fb07d7fc219218f98a91
Michal Sedivka <sedivka9108@gmail.com>
2024-02-12 19:23:10 +0100

    stripe-vercel-connection-refactor

--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const { mongoose } = require('mongoose')
-const cors = require('cors');
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const User = require('./models/userSchema')
-
-
-
 const dotenv = require('dotenv').config({ path: "../.env" })
 
+const express = require('express');
+// const { mongoose } = require('mongoose')
+const cors = require('cors');
+const bodyParser = require('body-parser')
+// const bcrypt = require('bcrypt')
+// const jwt = require('jsonwebtoken')
+// const User = require('./models/userSchema')
+
+
+
+
+const stripe = require('stripe')(process.env.STRIPE_API);
 
 // connect to express app
 const app = express();
 
 //db connection
-mongoose
-.connect(process.env.MONGO_URL, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+// mongoose
+// .connect(process.env.MONGO_URL, {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// })
 
-    .then(() => {
-        app.listen(3000, () => {
-            console.log("Server connected to port 3000 and MongoDb ")
-        })
+//     .then(() => {
+//         app.listen(3000, () => {
+//             console.log("Server connected to port 3000 and MongoDb ")
+//         })
       
-    })
-    .catch((error) => {
-        console.log('Unable to connect to Server and/or MongoDB', error)
-    })
+//     })
+//     .catch((error) => {
+//         console.log('Unable to connect to Server and/or MongoDB', error)
+//     })
 
 //middleware
 app.use(bodyParser.json())
@@ -43,50 +45,49 @@ app.use(express.json());
 
 //REGISTER
 //POST REGISTER
-app.post('/register', async (req, res) => {
-    try {
-        const { email, username, password } = req.body
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = new User({ email, username, password: hashedPassword })
-        await newUser.save()
-        res.status(201).json({ message: 'User created successfully' })
-    } catch (error) {
-        res.status(500).json({ error: 'Error signing up' })
-    }
-})
+// app.post('/register', async (req, res) => {
+//     try {
+//         const { email, username, password } = req.body
+//         const hashedPassword = await bcrypt.hash(password, 10)
+//         const newUser = new User({ email, username, password: hashedPassword })
+//         await newUser.save()
+//         res.status(201).json({ message: 'User created successfully' })
+//     } catch (error) {
+//         res.status(500).json({ error: 'Error signing up' })
+//     }
+// })
 
 //GET Registered Users
-app.get('/register', async (res) => {
-    try {
-        const users = await User.find()
-        res.status(201).json(users)
+// app.get('/register', async (res) => {
+//     try {
+//         const users = await User.find()
+//         res.status(201).json(users)
         
-    } catch (error) {
-        res.status(500).json({ error: 'Unable to get users' })
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({ error: 'Unable to get users' })
+//     }
+// })
 
-app.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body
-        const user = await User.findOne({ username })
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid Username'})
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password)
-        if(!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid Password' })
-        }
-        const token = jwt.sign({ userId: user._id },process.env.SECRET_KEY, { expiresIn: '1hr' })
-        res.json({ message: 'Login successful' })
-    } catch (error) {
-        res.status(500).json({ error: 'Error logging in' })
-    }
-})
+// app.post('/login', async (req, res) => {
+//     try {
+//         const { username, password } = req.body
+//         const user = await User.findOne({ username })
+//         if (!user) {
+//             return res.status(401).json({ error: 'Invalid Username'})
+//         }
+//         const isPasswordValid = await bcrypt.compare(password, user.password)
+//         if(!isPasswordValid) {
+//             return res.status(401).json({ error: 'Invalid Password' })
+//         }
+//         const token = jwt.sign({ userId: user._id },process.env.SECRET_KEY, { expiresIn: '1hr' })
+//         res.json({ message: 'Login successful' })
+//     } catch (error) {
+//         res.status(500).json({ error: 'Error logging in' })
+//     }
+// })
 
 
 
-const stripe = require('stripe')(process.env.STRIPE_API);
 
 app.post("/checkout", async (req, res) => {
     /*
@@ -110,20 +111,20 @@ app.post("/checkout", async (req, res) => {
 
 try {
     const items = req.body.items;
-    // let lineItems = [];
-    // items.forEach((item) => {
-    //     lineItems.push(
-    //         {
-    //             price: item.id,
-    //             quantity: item.quantity
-    //         }
-    //     )
-    // });
+    let lineItems = [];
+    items.forEach((item) => {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
     
-    let lineItems = items.map(item => ({
-        price: item.id,
-        quantity: item.quantity
-    }));
+    // let lineItems = items.map(item => ({
+    //     price: item.id,
+    //     quantity: item.quantity
+    // }));
 
 
 
@@ -143,10 +144,6 @@ try {
     console.error("Error processing checkout:", error);
     res.status(500).json({ error:'Error processing checkout' });
 }
-
-
-
-
 
 
 
