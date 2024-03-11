e53f904c654c5d7fd6d1739517fce1acfb20e716
michal9108 <sedivka9108@gmail.com>
2024-03-11 10:22:34 +0100

    renaming-components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import InfoSales from "@/scenes/HomePage/InfoSales";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Newsletter from "@/scenes/HomePage/Newsletter";
 import Reviews from "@/scenes/HomePage/Reviews";
-import Slider from '@/scenes/HomePage/Slider';
+import Slider from "@/scenes/HomePage/Slider";
 import Transformations from "@/scenes/HomePage/Transformations";
 import Shop from "@/scenes/HomePage/Shop";
 
