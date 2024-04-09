4c6c045f6b33b4ca7a0a643a18b2963ef0bd1258
michal9108 <sedivka9108@gmail.com>
2024-04-09 23:38:10 +0200

    refactored

--- src/scenes/HomePage/reviews.tsx
+++ src/scenes/HomePage/reviews.tsx
@@ -3,11 +3,10 @@ import { motion, useAnimation } from "framer-motion";
 import CardTemplate from "@/shared/CardTemplate";
 import { useState } from "react";
 import H2 from "@/shared/H2";
-import {useProductCart} from '../cart/ProductCartContext'
+import { useProductCart } from "../cart/ProductCartContext";
 
-export default function Reviews() {
-
-const {storeReviews} = useProductCart();
+const Reviews = () => {
+  const { storeReviews } = useProductCart();
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const controls = useAnimation();
@@ -37,27 +36,28 @@ const {storeReviews} = useProductCart();
                 <H2 color="primary-100">reviews</H2>
               </div>
             </div>
-                <div className="h-[480px] w-5/6 lg:w-4/6 mx-auto no-scrollbar  overflow-y-hidden relative">
+            <div className="h-[480px] w-5/6 lg:w-4/6 mx-auto no-scrollbar  overflow-y-hidden relative">
               <motion.ul
                 className="w-[2800px] whitespace-nowrap pl-0"
                 animate={controls}
               >
-                {storeReviews.length > 0 && storeReviews.map((item: ProductReviewType, index) => (
-                  <motion.li
-                    key={`${item.name}-${index}`}
-                    className="inline-block "
-                  >
-                    <CardTemplate
+                {storeReviews.length > 0 &&
+                  storeReviews.map((item: ProductReviewType, index) => (
+                    <motion.li
                       key={`${item.name}-${index}`}
-                      name={item.name}
-                      description={item.description}
-                      title={item.title}
-                      verifiedBuyer={item.verifiedBuyer}
-                      country={item.country}
-                      showStars={item.showStar}
-                    />
-                  </motion.li>
-                ))}
+                      className="inline-block "
+                    >
+                      <CardTemplate
+                        key={`${item.name}-${index}`}
+                        name={item.name}
+                        description={item.description}
+                        title={item.title}
+                        verifiedBuyer={item.verifiedBuyer}
+                        country={item.country}
+                        showStars={item.showStar}
+                      />
+                    </motion.li>
+                  ))}
               </motion.ul>
               <button
                 className=" z-30 absolute top-1/2 -translate-y-1/2 left-0 bg-gray-400 text-white rounded-full w-10 h-10 ml-6"
@@ -77,6 +77,6 @@ const {storeReviews} = useProductCart();
       </section>
     </div>
   );
-}
+};
 
-
+export default Reviews;
