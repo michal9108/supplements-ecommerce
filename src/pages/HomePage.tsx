6f4cb4940e2bfe2be5e332943eeb738bec7580e0
michal9108 <sedivka9108@gmail.com>
2024-04-09 23:41:08 +0200

    refactored

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import InfoSales from "@/scenes/HomePage/InfoSales";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Newsletter from "@/scenes/HomePage/Newsletter";
-import Reviews from "../scenes/HomePage/Reviews";
+import Reviews from "@/scenes/HomePage/Reviews";
 import HomeCarousel from "../scenes/HomePage/HomeCarousel";
 import Transformations from "@/scenes/HomePage/Transformations";
 import Shop from "@/scenes/HomePage/Shop";
